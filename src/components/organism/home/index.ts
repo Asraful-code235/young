33a82012d6a49a658abcf68c6c83bcefff1d96c5
Imran <mahmud.bubt.150@gmail.com>
2024-11-16 06:04:55 +0600

    added booking calender section

--- src/components/organism/home/index.ts
+++ src/components/organism/home/index.ts
@@ -7,6 +7,7 @@ import { HowTherapyWorks } from './HowTherapyWorks';
 import { WhyChooseUs } from './WhyChooseUs';
 import { InsuranceCoverage } from './InsuranceCoverage';
 import { PositiveOutcomes } from './PositiveOutcomes';
+import { BookingConsultation } from './BookingConsultation';
 
 export {
   HeroSection,
@@ -18,4 +19,5 @@ export {
   WhyChooseUs,
   InsuranceCoverage,
   PositiveOutcomes,
+  BookingConsultation,
 };
--- src/components/template/home/Home.tsx
+++ src/components/template/home/Home.tsx
@@ -1,4 +1,5 @@
 import {
+  BookingConsultation,
   HeroSection,
   HowPsychotherapyCanHelp,
   HowTherapyWorks,
@@ -22,6 +23,7 @@ export function Home() {
       <WhyChooseUs />
       <InsuranceCoverage />
       <PositiveOutcomes />
+      <BookingConsultation />
     </section>
   );
 }
